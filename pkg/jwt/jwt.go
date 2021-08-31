b6bb8bbe7256f55ed354dd3e8700720a7fb6e1c3
llucasreis <pro.lucasreis@gmail.com>
2021-08-31 18:00:51 -0300

    feat: add jwt functions

---
+++ pkg/jwt/jwt.go
@@ -0,0 +1,41 @@
+package jwt
+
+import (
+	"log"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var (
+	SecretKey = []byte("secret")
+)
+
+func GenerateToken(username string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["username"] = username
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	tokeString, err := token.SignedString(SecretKey)
+	if err != nil {
+		log.Fatal("Error in Generating Key")
+		return "", err
+	}
+
+	return tokeString, nil
+}
+
+func ParseToken(tokenStr string) (string, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return SecretKey, nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		username := claims["username"].(string)
+		return username, nil
+	} else {
+		return "", err
+	}
+}
