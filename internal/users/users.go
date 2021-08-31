3e96f6856f672916a931f8fdb3ed6c72c7de5054
llucasreis <pro.lucasreis@gmail.com>
2021-08-31 18:01:01 -0300

    feat: add user use cases

--- internal/users/users.go
+++ internal/users/users.go
@@ -1,7 +1,81 @@
 package users
 
+import (
+	"database/sql"
+	"log"
+
+	database "github.com/llucasreis/go-blog/internal/pkg/db/mysql"
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	ID       string `json:"id"`
 	Username string `json:"name"`
 	Password string `json:"password"`
+}
+
+func (user *User) Create() {
+	stmt, err := database.Db.Prepare("INSERT INTO Users(Username, Password) VALUES(?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hashedPassword, err := HashPassword(user.Password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = stmt.Exec(user.Username, hashedPassword)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (user *User) Authenticate() bool {
+	stmt, err := database.Db.Prepare("select Password from Users WHERE Username = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	row := stmt.QueryRow(user.Username)
+
+	var hashedPassword string
+	err = row.Scan(&hashedPassword)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	return CheckPasswordHash(user.Password, hashedPassword)
+}
+
+func GetUserIdByUsername(username string) (int, error) {
+	stmt, err := database.Db.Prepare("select ID from Users WHERE Username = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	row := stmt.QueryRow(username)
+
+	var Id int
+	err = row.Scan(&Id)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			log.Println(err)
+		}
+		return 0, err
+	}
+
+	return Id, nil
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
